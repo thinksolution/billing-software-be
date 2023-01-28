b9454047fbb292596feed33482eb7242514fc6aa
thinksolution <thinksolution2014@gmail.com>
2023-01-29 03:09:29 +0530

    code refactored

---
+++ services/BL/companyBL.js
@@ -0,0 +1,53 @@
+const { encryptText } = require('../../utils/helper/bcrypt.decrypt')
+const { generateJwtToken } = require('../../utils/helper/tokenGenerator')
+
+const jwt = require('jsonwebtoken')
+const {tokenVerifier} = require('../../utils/helper/tokenGenerator')
+const {
+    registerCompany,
+    getCompanyByEmail,
+    loginCompany,getTokenByEmail
+} = require('../DAL/companyDL')
+
+
+const registerCompanyBL = async (req, res) => {
+
+    try {
+        console.log('BL hit')
+        let data = req.body
+        const userExist = await getCompanyByEmail(data)
+        console.log(userExist)
+        if(userExist.length>0){
+            console.log('ue hit')
+            return res.status(200).send("User Already Exist")
+        }
+        data.password = await encryptText(data.password)
+        const token = generateJwtToken(data)
+        data.registrationKey = token;
+        let saveCompany = await registerCompany(data);
+        res.locals.rootdata = saveCompany;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const loginCompanyBL = async (req, res) => {
+
+    try {
+        const data = req.body;
+        const token = await getTokenByEmail(data);
+        const authorized = tokenVerifier(token);
+        if(!authorized){
+            return res.status(401).send('Unauthorized User');
+        }
+        const loginCompanyResult = await loginCompany(data)
+        res.locals.rootdata = loginCompanyResult;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    registerCompanyBL,
+    loginCompanyBL,
+}
---
+++ services/DAL/companyDL.js
@@ -0,0 +1,51 @@
+const RegistrationModel = require("../../utils/mongodb/model/registration.model");
+const { decryptText } = require("../../utils/helper/bcrypt.decrypt");
+
+const getCompanyByEmail = async (data) => {
+  const _data = await RegistrationModel.find({ email: data.email });
+  console.log("_data", _data);
+  return _data;
+};
+const registerCompany = async (data) => {
+  try {
+    let companyData = new RegistrationModel(data);
+    return companyData.save();
+  } catch (error) {
+    throw error;
+  }
+};
+const getTokenByEmail = async (data) => {
+  let companyData = undefined;
+  if (data !== undefined) {
+    companyData = await RegistrationModel.find({ email: data.email });
+  }
+  const result = companyData[0].registrationKey;
+  
+  return result;
+};
+const loginCompany = async (data) => {
+  try {
+    const loginResult = await RegistrationModel.find({ email: data.email });
+    if (loginResult.length != 0) {
+      const passwordMatch = await decryptText(
+        data.password,
+        loginResult[0].password
+      );
+      if (passwordMatch) {
+        return loginResult;
+      } else {
+        return { error: "User not Exist" };
+      }
+    } else {
+      return { error: "User not Exist" };
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+module.exports = {
+  getCompanyByEmail,
+  registerCompany,
+  loginCompany,
+  getTokenByEmail
+};
